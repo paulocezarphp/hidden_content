768fb97a500a914c326fb7c8b2b3167f3c2c36a5
Cezar Leigos <cezarleigos28@outlook.com>
2021-07-20 20:29:19 -0300

    v0.3 - Creating Roles
    This time we start creating the functions that we will use in the future, they will be implemented, the functions that have _load at the end are the functions that are loaded during page load and cannot be changed during page execution, as functions that do not have this parameter can change the display of content at runtime.

---
+++ imports/hidden_content.js
@@ -0,0 +1,45 @@
+
+function detect_element(element_id_class){
+
+}
+
+function context_blur(type, element_id_class){ //low, medium, high
+   
+
+}
+
+function context_blur_load(type, element_id_class){ // (#) id, (.) class
+   
+
+}
+
+function context_cryptography(type, element_id_class){
+
+
+}
+
+function context_cryptography_load(type, element_id_class){
+
+
+}
+
+function context_hash(type, element_id_class){
+
+
+}
+
+function context_hash_load(type, element_id_class){
+
+
+}
+
+function context_shuffle(type, element_id_class){
+
+
+}
+
+function context_shuffle_load(){
+
+
+}
+
